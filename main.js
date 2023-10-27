dd21e62b025f517551733b35b71dccb1211051d7
nanuka mindadze <nanukamindadze@gmail.com>
2023-10-27 15:57:45 +0400

    davaleba

---
+++ main.js
@@ -0,0 +1,7 @@
+const brg = document.getElementById("hidden2");
+const brg1 = document.querySelector('.burgermenu'); 
+
+brg.addEventListener('click', function() {
+    brg1.classList.toggle('active'); 
+    console.log ('nanuka')
+});
